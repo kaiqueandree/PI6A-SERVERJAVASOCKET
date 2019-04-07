40c0ad4c8a66f7116adbdf2f6eca8ceaf7c2eb14
lucasvpontes <48896896+lucasvpontes@users.noreply.github.com>
2019-04-07 19:22:32 -0300

    Update ClienteService.java
    Realizado alteração referente a ultima entrega.

--- src/br/com/pi/service/ClienteService.java
+++ src/br/com/pi/service/ClienteService.java
@@ -1,5 +1,23 @@
 package br.com.pi.service;
 
-public class ClienteService {
+import java.sql.SQLException;
 
+import br.com.pi.dao.ServidorDAO;
+import br.com.pi.model.ServidorWeb;
+
+
+public class ServidorService {
+
+	ServidorDAO objectDAO;
+	
+	public ServidorService() throws SQLException {
+		 ServidorDAO objectDAO = new ServidorDAO();
+		   this.objectDAO = objectDAO;
+	   }
+		
+		public void inserir(ServidorWeb serv) throws SQLException {
+			objectDAO.inserir(serv);
+		}
+	
+	
 }
